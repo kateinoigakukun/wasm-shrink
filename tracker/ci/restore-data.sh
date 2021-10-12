c9b7a87a0940d6282445253a2661fac07d33ca90
Yuta Saito <kateinoigakukun@gmail.com>
2021-10-12 17:03:41 +0900

    ci: enable git lfs and checkout remote branch

--- tracker/ci/restore-data.sh
+++ tracker/ci/restore-data.sh
@@ -2,5 +2,5 @@
 
 repo_root="$(cd "$(dirname "$0")/../.." && pwd)"
 cd "$repo_root"
-git checkout gh-pages ./data.json
+git checkout origin/gh-pages ./data.json
 mv $repo_root/data.json $repo_root/tracker/html/data.json
