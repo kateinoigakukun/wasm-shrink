4e5bedbc32a71119465714466c46b94a068fb552
Yuta Saito <kateinoigakukun@gmail.com>
2021-10-12 16:53:23 +0900

    ci: setup continuous file size tracking

--- tracker/ci/bench.sh
+++ tracker/ci/bench.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cargo run --bin wasm-shrink-tracker update .. --data html/data.json
+tracker_dir="$(cd "$(dirname "$0")/.." && pwd)"
+cd "$tracker_dir"
+cargo run --bin wasm-shrink-tracker update $tracker_dir/.. --data html/data.json
