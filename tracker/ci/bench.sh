b78df967c4461d239ce63e108f2baf1ed710e72e
Yuta Saito <kateinoigakukun@gmail.com>
2021-10-24 12:58:42 +0900

    ci: update data directory structure

--- tracker/ci/bench.sh
+++ tracker/ci/bench.sh
@@ -2,4 +2,4 @@
 
 tracker_dir="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$tracker_dir"
-cargo run --bin wasm-shrink-tracker update $tracker_dir/.. --data html/data.json
+cargo run --bin wasm-shrink-tracker update $tracker_dir/.. --data html/data/swift-hello.wasm.json
--- tracker/ci/restore-data.sh
+++ tracker/ci/restore-data.sh
@@ -2,5 +2,5 @@
 
 repo_root="$(cd "$(dirname "$0")/../.." && pwd)"
 cd "$repo_root"
-git checkout origin/gh-pages ./data.json
-mv $repo_root/data.json $repo_root/tracker/html/data.json
+git checkout origin/gh-pages ./data
+mv $repo_root/data $repo_root/tracker/html/data
