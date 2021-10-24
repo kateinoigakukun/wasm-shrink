a9717420e012ad42f5ea8d3efc537c291d5a07f1
Yuta Saito <kateinoigakukun@gmail.com>
2021-10-24 13:32:11 +0900

    fix: restore all json record data

--- tracker/ci/restore-data.sh
+++ tracker/ci/restore-data.sh
@@ -3,4 +3,4 @@
 repo_root="$(cd "$(dirname "$0")/../.." && pwd)"
 cd "$repo_root"
 git checkout origin/gh-pages ./data
-mv $repo_root/data $repo_root/tracker/html/data
+mv $repo_root/data/*.json $repo_root/tracker/html/data
